e9f12d56226ad159f7318c11000487727960dd5a
David Thong Nguyen <dev@davidtn.com>
2022-01-24 13:52:42 +0700

    🏷️ Add cloud upload response interface

---
+++ src/interfaces/cloudinary-upload-response.interface.ts
@@ -0,0 +1,14 @@
+export interface CloudinaryUploadResponse {
+  public_id: string;
+  version: number;
+  signature: string;
+  width: number;
+  height: number;
+  format: string;
+  resource_type: string;
+  created_at: string;
+  bytes: number;
+  type: string;
+  url: string;
+  secure_url: string;
+}
