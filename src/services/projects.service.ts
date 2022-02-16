df923df07511e616d1a841d95960c072279c7243
David Thong Nguyen <dev@davidtn.com>
2022-02-16 10:51:22 -0500

    🧑‍💻 Service function should handle required params

--- src/services/projects.service.ts
+++ src/services/projects.service.ts
@@ -3,18 +3,23 @@ import { Project } from '@interfaces/project.interface';
 import { User } from '@interfaces/users.interface';
 import projectModel from '@models/projects.model';
 import userModel from '@models/users.model';
+import { Request } from 'express';
 
 class ProjectsService {
   public users = userModel;
   public projects = projectModel;
 
-  public async getProjectByCreatorAndName(username, projectname): Promise<Project> {
+  public async getProjectByCreatorAndName(req: Request): Promise<Project> {
+    const username: string = (req.params.username as string).toLocaleLowerCase();
+    const projectname: string = (req.params.projectname as string).toLocaleLowerCase();
     const user: User = await this.users.findOne({ username });
+
     // TODO: what if user is deleted and the project can't be found this way?
     if (!user?._id) throw new HttpException(404, `User ${username} does not exist`);
 
     // TODO: access check, is this project public or does it belong to the user
     const findProjectByNameData: Project = await this.projects.findOne({ name: projectname, creator: user._id });
+
     if (!findProjectByNameData) throw new HttpException(404, `Project ${projectname} by ${username} does not exist`);
     return findProjectByNameData;
   }
