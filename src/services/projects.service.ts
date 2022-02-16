448fd298bd10bba687b6de3250400e5fcf4069d3
David Thong Nguyen <dev@davidtn.com>
2022-02-16 10:22:26 -0500

    🚧♻️ Adding db logic to ProjectsService

--- src/services/projects.service.ts
+++ src/services/projects.service.ts
@@ -1,22 +1,41 @@
-class ProjectsService {
-  public generateUniformProjectName(projectName: string) {
-    const result = projectName
-      .trim()
-      .toLocaleLowerCase()
-      .split(' ')
-      .reduce((prev, curr, index) => {
-        let res = '';
-        if (index > 0) {
-          res += '-';
-        }
-        return prev + res + curr;
-      }, '');
-    return result;
-  }
+import { HttpException } from '@exceptions/HttpException';
+import { Project } from '@interfaces/project.interface';
+import { User } from '@interfaces/users.interface';
+import projectModel from '@models/projects.model';
+import userModel from '@models/users.model';
 
-  generateProjectPath(username, projectName) {
-    return username + '/' + this.generateUniformProjectName(projectName);
+class ProjectsService {
+  public users = userModel;
+  public projects = projectModel;
+
+  public async getProjectByCreatorAndName(username, projectname): Promise<Project> {
+    const user: User = await this.users.findOne({ username });
+    // TODO: what if user is deleted and the project can't be found this way?
+    if (!user?._id) throw new HttpException(404, `User ${username} does not exist`);
+
+    // TODO: access check, is this project public or does it belong to the user
+    const findProjectByNameData: Project = await this.projects.findOne({ name: projectname, creator: user._id });
+    if (!findProjectByNameData) throw new HttpException(404, `Project ${projectname} by ${username} does not exist`);
+    return findProjectByNameData;
   }
+  // public generateUniformProjectName(projectName: string) {
+  //   const result = projectName
+  //     .trim()
+  //     .toLocaleLowerCase()
+  //     .split(' ')
+  //     .reduce((prev, curr, index) => {
+  //       let res = '';
+  //       if (index > 0) {
+  //         res += '-';
+  //       }
+  //       return prev + res + curr;
+  //     }, '');
+  //   return result;
+  // }
+
+  // generateProjectPath(username, projectName) {
+  //   return username + '/' + this.generateUniformProjectName(projectName);
+  // }
 }
 
 export default ProjectsService;
