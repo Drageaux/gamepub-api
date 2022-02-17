13d81ca6816324ba6e696ac6fe6e3d2db8d256a4
David Thong Nguyen <dev@davidtn.com>
2022-02-17 16:40:10 -0500

    🐛 Should turn result into plain object before modifying

--- src/interfaces/job.interface.ts
+++ src/interfaces/job.interface.ts
@@ -4,7 +4,7 @@ import { Project } from './project.interface';
 export interface Job {
   _id: Types.ObjectId;
   project: Types.ObjectId | Project; // Project._id
-  jobNumber: number;
+  jobNumber?: number;
   title: string;
   body?: string;
   imageUrl?: string;
--- src/services/jobs.service.ts
+++ src/services/jobs.service.ts
@@ -1,15 +1,15 @@
 import { Job } from '@/interfaces/job.interface';
 import jobModel from '@/models/jobs.model';
+import { Document } from 'mongoose';
 
 class JobsService {
   public jobs = jobModel;
 
   public async getJobsWithNumbers(projectId: string): Promise<Job[]> {
-    const findJobsByProject: Job[] = await this.jobs.find({ project: projectId }).sort({ createdAt: 'asc' });
+    const findJobsByProject: (Job & Document)[] = await this.jobs.find({ project: projectId }).sort({ createdAt: 'asc' });
 
-    return findJobsByProject.map((x: Job, index) => {
-      x.jobNumber = index + 1;
-      return x;
+    return findJobsByProject.map((x, index) => {
+      return { ...x.toObject(), jobNumber: index + 1 };
     });
   }
 }
