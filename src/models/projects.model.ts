c5dd33a8acc9be4ccaaa66b1256b432f3cce378e
David Thong Nguyen <dev@davidtn.com>
2022-02-22 11:27:30 -0500

    🗃️ Project holds jobs in an array

--- src/models/projects.model.ts
+++ src/models/projects.model.ts
@@ -1,5 +1,6 @@
 import { model, Schema, Document } from 'mongoose';
 import { Project } from '@/interfaces/project.interface';
+import jobModel from './jobs.model';
 
 const projectSchema: Schema = new Schema({
   creator: { type: Schema.Types.ObjectId, ref: 'User', required: true },
@@ -9,6 +10,7 @@ const projectSchema: Schema = new Schema({
   imageUrl: String,
   tags: [String],
   description: String,
+  jobs: [{ type: Schema.Types.ObjectId, ref: 'Job' }],
 }).index({ creator: 1, name: 1 }, { unique: true });
 
 const projectModel = model<Project & Document>('Project', projectSchema);
