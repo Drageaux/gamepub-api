ac7cd10c1f8b6631b07ad84c0f38c75bd2d15b35
David Thong Nguyen <dev@davidtn.com>
2022-02-22 11:26:21 -0500

    🏷️ Project interface extends Document

--- src/interfaces/project.interface.ts
+++ src/interfaces/project.interface.ts
@@ -1,11 +1,14 @@
-import { Types } from 'mongoose';
+import { Document, Types } from 'mongoose';
 import { User } from './users.interface';
+import { Mongoose } from 'mongoose';
+import { Job } from './job.interface';
 
-export interface Project {
+export interface Project extends Document {
   _id: Types.ObjectId;
   creator: Types.ObjectId | User; // User._id
   name: string;
   displayName?: string;
   githubRepo?: string;
   imageUrl?: string;
+  jobs?: Types.DocumentArray<Job>;
 }
