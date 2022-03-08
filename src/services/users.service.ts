47b11ecc635245a5b7ece08559b21f65f91b4c7a
David Thong Nguyen <dev@davidtn.com>
2022-03-08 00:25:07 -0500

    🔥 Remove old secret

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -10,17 +10,6 @@ const auth0 = new ManagementClient({
   clientSecret,
   scope: 'read:users update:users',
 });
-// const options: AxiosRequestConfig = {
-//   method: 'POST',
-//   url: 'https://gamepub-dev.us.auth0.com/oauth/token',
-//   headers: { 'Content-Type': 'application/json' },
-//   data: {
-//     client_id: 'FdR0Fv4GojpY4cAd0SZoWcxJXydYsHy1',
-//     client_secret: '-KBzmEEvS95Sb3d_kfmcMuuBsKqkFDdG07-QT1W4WkyoYP7K8LnmsAWaDvNB4zc6',
-//     audience: 'https://gamepub-dev.us.auth0.com/api/v2/',
-//     grant_type: 'client_credentials',
-//   },
-// };
 
 class UserService {
   public findUserByUsername = async (username: string) => {
