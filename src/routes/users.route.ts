8423c0314e4d3f4920c1751e2bdd88be9c8bd565
David Thong Nguyen <dev@davidtn.com>
2022-03-08 00:19:27 -0500

    🛂✨ Using new auth flow in UserService

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -14,12 +14,12 @@ class UsersRoute implements Routes {
   }
 
   private initializeRoutes() {
-    this.router.get(`${this.path}`, this.usersController.getUsers);
+    // this.router.get(`${this.path}`, this.usersController.getUsers);
     this.router.get(`${this.path}/:username`, this.usersController.getUserByUsername);
     // this.router.get(`${this.path}/:id`, this.usersController.getUserById);
-    this.router.post(`${this.path}`, validationMiddleware(CreateUserDto, 'body'), this.usersController.createUser);
-    this.router.put(`${this.path}/:id`, validationMiddleware(CreateUserDto, 'body', true), this.usersController.updateUser);
-    this.router.delete(`${this.path}/:id`, this.usersController.deleteUser);
+    // this.router.post(`${this.path}`, validationMiddleware(CreateUserDto, 'body'), this.usersController.createUser);
+    // this.router.put(`${this.path}/:id`, validationMiddleware(CreateUserDto, 'body', true), this.usersController.updateUser);
+    // this.router.delete(`${this.path}/:id`, this.usersController.deleteUser);
   }
 }
 
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,9 +1,37 @@
-import bcrypt from 'bcrypt';
-import { CreateUserDto } from '@dtos/users.dto';
-import { HttpException } from '@exceptions/HttpException';
-import { User } from '@interfaces/users.interface';
-import userModel from '@models/users.model';
-import { isEmpty } from '@utils/util';
+import config from 'config';
+import { Auth0Config } from '@/interfaces/auth0-config.interface';
+import { ManagementClient } from 'auth0';
 
-class UserService {}
+const { issuerBaseUrl, clientId, clientSecret }: Auth0Config = config.get('auth0');
+const auth0 = new ManagementClient({
+  domain: issuerBaseUrl,
+  clientId,
+  clientSecret,
+  scope: 'read:users update:users',
+});
+// const options: AxiosRequestConfig = {
+//   method: 'POST',
+//   url: 'https://gamepub-dev.us.auth0.com/oauth/token',
+//   headers: { 'Content-Type': 'application/json' },
+//   data: {
+//     client_id: 'FdR0Fv4GojpY4cAd0SZoWcxJXydYsHy1',
+//     client_secret: '-KBzmEEvS95Sb3d_kfmcMuuBsKqkFDdG07-QT1W4WkyoYP7K8LnmsAWaDvNB4zc6',
+//     audience: 'https://gamepub-dev.us.auth0.com/api/v2/',
+//     grant_type: 'client_credentials',
+//   },
+// };
+
+class UserService {
+  public findUserByUsername = async (username: string) => {
+    try {
+      const users = await auth0.getUsers({ q: 'username=' + username });
+      console.log('test', users);
+
+      return users[0];
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  };
+}
 export default UserService;
