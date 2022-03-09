b93686469ad388517fe779c710c6f288e3df5333
David Thong Nguyen <dev@davidtn.com>
2022-03-09 00:26:35 -0500

    ⚡ Don't instantiate another auth0 client with userService

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -5,24 +5,31 @@ import { HttpException } from '@/exceptions/HttpException';
 
 const { issuerBaseUrl, clientId, clientSecret }: Auth0Config = config.get('auth0');
 
-class UserService {
-  private auth0 = new ManagementClient({
-    domain: issuerBaseUrl,
-    clientId,
-    clientSecret,
-    scope: 'read:users update:users',
-  });
+const auth0 = new ManagementClient({
+  domain: issuerBaseUrl,
+  clientId,
+  clientSecret,
+  scope: 'read:users update:users read:roles read:role_members',
+});
+auth0.getAccessToken().then(console.log).catch(console.error);
 
+class UserService {
   public findUserByUsername = async (username: string): Promise<User> => {
-    const users = await this.auth0.getUsers({ q: 'username=' + username });
+    const users = await auth0.getUsers({ q: 'username=' + username });
     if (users.length === 0) throw new HttpException(404, `User ${username} not found.`);
 
     return users[0];
   };
 
   public findUserById = async (id: string): Promise<User> => {
-    const user = await this.auth0.getUser({ id });
+    const user = await auth0.getUser({ id });
+    if (!user) throw new HttpException(404, `User not found.`);
+
     return user;
+  };
+
+  public getUserRoles = async (id: string) => {
+    return await auth0.getUserRoles({ id });
   };
 }
 export default UserService;
