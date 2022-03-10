9b1d82e235504265a4058a2b85fa926bc1e7aa3d
David Thong Nguyen <dev@davidtn.com>
2022-03-09 19:04:11 -0500

    🔊 Add logging error if 500 fail code

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -7,6 +7,10 @@ const errorMiddleware = (error: HttpException, req: Request, res: Response, next
     const status: number = error.status || 500;
     const message: string = error.message || 'Something went wrong';
 
+    if (error.status == 500) {
+      console.error(error);
+    }
+
     logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`);
     res.status(status).json({ message });
   } catch (error) {
