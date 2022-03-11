57d59742af854bd88b73b3d28ac4a68d4666c6ee
David Thong Nguyen <dev@davidtn.com>
2022-03-11 15:58:13 -0500

    🗃️ Require project and job id in job comment

--- src/models/job-comments.model.ts
+++ src/models/job-comments.model.ts
@@ -3,8 +3,8 @@ import { JobComment } from '@interfaces/job.interface';
 
 const jobCommentSchema: Schema = new Schema(
   {
-    // project: { type: Schema.Types.ObjectId, ref: 'Project', required: true },
-    // job: { type: Schema.Types.ObjectId, ref: 'Job', required: true },
+    project: { type: Schema.Types.ObjectId, ref: 'Project', required: true },
+    job: { type: Schema.Types.ObjectId, ref: 'Job', required: true },
     user: { type: String, required: true },
     body: { type: String, required: true },
   },
