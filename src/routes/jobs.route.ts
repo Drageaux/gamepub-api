e2321cb39f1b1b965ceb64cda4abf9c580cc381f
David Thong Nguyen <dev@davidtn.com>
2022-03-10 21:06:26 -0500

    🛂 Validating params in job routes

--- src/routes/jobs.route.ts
+++ src/routes/jobs.route.ts
@@ -5,6 +5,7 @@ import JobsController from '@/controllers/jobs.controller';
 import validationMiddleware from '@/middlewares/validation.middleware';
 import { CreateJobDto } from '@/dtos/jobs.dto';
 import { softCheckUser, injectUsername, requireUser } from '@/middlewares/auth.middleware';
+import { JobNumberPathParams, ProjectPathParams } from '@/dtos/params.dto';
 
 class JobsRoute implements Routes {
   public path = '/jobs';
@@ -25,12 +26,14 @@ class JobsRoute implements Routes {
       `/users/:username/projects/:projectname${this.path}`,
       softCheckUser,
       injectUsername,
+      validationMiddleware(ProjectPathParams, 'params'),
       this.jobsController.getJobsByProjectFullPath,
     );
     this.router.get(
       `/users/:username/projects/:projectname${this.path}/:jobnumber`,
       softCheckUser,
       injectUsername,
+      validationMiddleware(JobNumberPathParams, 'params'),
       this.jobsController.getJobByJobNumber,
     );
     // this.router.get(`/projects/:projectid${this.path}`, this.jobsController.getJobsByProjectId);
@@ -40,6 +43,7 @@ class JobsRoute implements Routes {
       `/users/:username/projects/:projectname${this.path}`,
       requireUser,
       injectUsername,
+      validationMiddleware(ProjectPathParams, 'params'),
       validationMiddleware(CreateJobDto, 'body'),
       this.jobsController.createJob,
     );
