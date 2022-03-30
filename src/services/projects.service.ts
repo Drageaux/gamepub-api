d86225b1717bd832f4aa42d1cf3f9fe1bc5cd1d8
David Thong Nguyen <dev@davidtn.com>
2022-03-30 16:44:41 -0400

    ✨ Move function that is shared among resources to UserService

--- src/services/projects.service.ts
+++ src/services/projects.service.ts
@@ -1,7 +1,6 @@
 import { HydratedDocument } from 'mongoose';
 import { HttpException } from '@exceptions/HttpException';
 import { Project } from '@interfaces/project.interface';
-import { User } from '@interfaces/users.interface';
 import projectModel from '@models/projects.model';
 import userModel from '@models/users.model';
 import { RequestWithUser } from '@/interfaces/auth.interface';
@@ -28,9 +27,6 @@ class ProjectsService {
     return findProjectByNameData;
   }
 
-  public getPrivateQueryOptions = (isUser: boolean) => {
-    return isUser ? { $in: [true, false, undefined, null] } : { $ne: true };
-  };
   // public generateUniformProjectName(projectName: string) {
   //   const result = projectName
   //     .trim()
