4b1bca9bdc8ac82c373a7a82f2d29be270145c6a
David Thong Nguyen <dev@davidtn.com>
2022-04-08 19:43:43 -0400

    🚑 Hot fix to use correct references

--- src/models/job-submissions.model.ts
+++ src/models/job-submissions.model.ts
@@ -4,14 +4,15 @@ import jobModel from './jobs.model';
 
 const jobSubmissionSchema: Schema = new Schema(
   {
-    submissionNumber: { type: Number, default: 1 },
     job: { type: Schema.Types.ObjectId, ref: 'Job', required: true },
     user: { type: String, required: true },
     githubRepo: { type: String, required: true, trim: true },
     description: String,
+    // counters
+    submissionNumber: { type: Number, default: 1 },
   },
   { timestamps: true },
-).index({ job: 1, jobNumber: 1 }, { unique: true });
+).index({ job: 1, submissionNumber: 1 }, { unique: true });
 
 jobSubmissionSchema.pre('save', async function (next) {
   if (!this.isNew) {
@@ -19,11 +20,11 @@ jobSubmissionSchema.pre('save', async function (next) {
     return;
   }
 
-  // auto increment job count
+  // auto increment submission count
   try {
     const updatedJob: Job = await jobModel.findByIdAndUpdate(this.job, { $inc: { submissionsCount: 1 } }, { new: true });
     const submissionNumber = updatedJob.submissionsCount;
-    this.jobNumber = submissionNumber;
+    this.submissionNumber = submissionNumber;
     next();
   } catch (err) {
     next(err);
