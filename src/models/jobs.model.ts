4970c801d9e4336b623d7737db9548281512427e
David Thong Nguyen <dev@davidtn.com>
2022-04-08 17:46:44 -0400

    🚑 Adding missing JobSubmission interface

--- src/models/jobs.model.ts
+++ src/models/jobs.model.ts
@@ -8,8 +8,9 @@ const jobSchema: Schema = new Schema(
     title: { type: String, required: true, trim: true },
     body: { type: String, trim: true },
     imageUrl: { type: String, trim: true },
-    comments: [{ type: Schema.Types.ObjectId, ref: 'JobComment' }],
     subscribers: [String],
+    // commentsCount: { type: Number, default: 0 },
+    // submissionsCount: { type: Number, default: 0 },
   },
   { timestamps: true, toJSON: { virtuals: true }, toObject: { virtuals: true } },
 ).index({ project: 1, jobNumber: 1 }, { unique: true });
