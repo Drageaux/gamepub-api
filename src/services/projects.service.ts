13edd0cd337eb08041f8d5e190a6bfed6836a7b9
David Thong Nguyen <dev@davidtn.com>
2022-04-12 21:06:12 -0400

    🛂 Access check; alternative way to update project

--- src/services/projects.service.ts
+++ src/services/projects.service.ts
@@ -25,20 +25,36 @@ class ProjectsService {
     return findProjectByNameData;
   }
 
+  public async updateProjectById(req: RequestWithUser, update): Promise<HydratedDocument<Project>> {
+    if (!req.username) throw new HttpException(401, `Unauthorized`);
+
+    const findProject = await this.projects.findById(req.params.id);
+    if (!findProject) throw new HttpException(404, `Project with ID ${req.params.id} does not exist`);
+
+    // TODO: allow collaborators to edit
+    // does it belong to the user
+    if (req.username !== findProject.creator) throw new HttpException(401, `Unauthorized`);
+
+    const findProjectByNameData = await this.projects.findByIdAndUpdate(req.params.id, update);
+
+    return findProjectByNameData;
+  }
+
   public async updateProjectByCreatorAndName(req: RequestWithUser, update): Promise<HydratedDocument<Project>> {
     const username: string = (req.params.username as string).toLocaleLowerCase();
     const projectname: string = (req.params.projectname as string).toLocaleLowerCase();
 
-    // TODO: what if user is deleted and the project can't be found this way?x
+    // TODO: allow collaborators to edit
+    // TODO: what if user is deleted and the project can't be found this way?
 
-    // TODO: access check, is this project public or does it belong to the user
+    // access check, does it belong to the user
     const isUser = username === req.username;
-    const findProjectByNameData = await this.projects.findByIdAndUpdate({ name: projectname, creator: username }, update);
+    if (!isUser) throw new HttpException(401, `You do not have access to this project`);
 
+    const findProjectByNameData = await this.projects.findOneAndUpdate({ name: projectname, creator: username }, update, {
+      returnOriginal: false,
+    });
     if (!findProjectByNameData) throw new HttpException(404, `Project ${projectname} by ${username} does not exist`);
-    if (findProjectByNameData.private && !isUser) {
-      throw new HttpException(401, `You do not have access to this project`);
-    }
 
     return findProjectByNameData;
   }
